b41918066664b1d63d520bbc60427f54bb48643a
ignitz <ignitzhjfk@gmail.com>
2016-04-03 13:03:15 -0300

    Example of Command Line to execute via (Extended Toolkit CC) to automate scripts.

---
+++ InDesign/Execute ExtendedScript Toolkit Example.jsx
@@ -0,0 +1,16 @@
+﻿/*
+2016 Yuri Niitsuma
+Open Indesign with Extednded Toolkit CC
+*/
+
+#target indesign // Default InDesign
+// #target indesign11 // InDesign 2015
+
+// Open Indesign
+indesign.activate();
+
+// Open a new File
+indesign.openAsNew();
+
+// Close InDesign
+indesign.quit();
